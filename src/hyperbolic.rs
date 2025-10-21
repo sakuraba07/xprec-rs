27da4645b0c9c3a122f9e516093bd9102de20570
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2025-10-21 16:33:43 +0200

    Add tanh in new cool implementation.o

--- src/hyperbolic.rs
+++ src/hyperbolic.rs
@@ -73,6 +73,28 @@ pub fn sinh(x: d64) -> d64
     return copysign(subfast_qq(exp_x_half, exp_mx_half), x);
 }
 
+pub fn tanh(x: d64) -> d64
+{
+    let xx = abs(x);
+    if !(xx.hi <= 36.5) {
+        if is_nan(x) {
+            return d64::NAN;
+        } else {
+            // Asymptotically, we have +- 1
+            return copysign(d64::from(1.0), x);
+        }
+    }
+
+    // For small x, the naive tanh(x) leads to cancellation. Again, we rewrite
+    // it. Let exp(x) = 1 + y, then:
+    //
+    //    tanh(x) = z / (2 + z)  with  z = y * (2 + y)
+    //
+    // This is stable along the entire range.
+    let y = expm1(xx);
+    let z = y * (2.0 + y);
+    return copysign(z / (2.0 + z), x);
+}
 
 #[cfg(test)]
 mod test{
@@ -110,7 +132,7 @@ mod test{
         assert!(is_finite(cosh(d64::from(COSH_MAX))));
     }
 
-        #[test]
+    #[test]
     fn test_sinh()
     {
         // special values
@@ -140,5 +162,35 @@ mod test{
         }
 
         assert!(is_finite(sinh(d64::from(COSH_MAX))));
+    }
+
+    #[test]
+    fn test_tanh()
+    {
+        // special values
+        assert!(tanh(d64::from(1000.0)) == d64::from(1.0));
+        assert!(tanh(d64::from(-1000.0)) == d64::from(-1.0));
+        assert!(tanh(d64::INFINITY) == d64::from(1.0));
+        assert!(tanh(-d64::INFINITY) == d64::from(-1.0));
+        assert!(is_nan(tanh(d64::NAN)));
+
+        // simple vals
+        check_unary(tanh, |x| x.tanh(), d64::from(0.0), 1.0);
+
+        // small values
+        let mut x = d64::from(1.0);
+        while x.hi > 1e-290 {
+            check_unary(tanh, |x| x.tanh(), x, 2.0);
+            check_unary(tanh, |x| x.tanh(), -x, 2.0);
+            x *= 0.947;
+        }
+
+        // large values
+        x = d64::from(1.0);
+        while x.hi < 10.0 * COSH_MAX {
+            check_unary(tanh, |x| x.tanh(), x, 2.0);
+            check_unary(tanh, |x| x.tanh(), -x, 2.0);
+            x *= 1.0041;
+        }
     }
 }
