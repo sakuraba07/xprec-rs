0e801c1623faef6ac56846646515ef41f51ac2db
SatoshiTerasaki <terasakisatoshi.math@gmail.com>
2025-10-28 10:10:02 +0900

    Define kk as a type of Df64

--- src/gauss.rs
+++ src/gauss.rs
@@ -58,9 +58,9 @@ fn plx(ell: i64, x: Df64) -> (Df64, Df64)
     // Bonnet's recursion formula
     for k in 1..ell {
         // next term
-        let kk = k as f64;
         let p2 = ((2.0 * kk + 1.0) * x * p1 - kk * p0) / (kk + 1.0);
 
+        let kk: Df64 = Df64::from(k as f64);
         // shift terms by one
         p0 = p1;
         p1 = p2;
